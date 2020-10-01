9da310acec3de46ccaf68a0844a4a8f9ad713b5f
Fa-So <Fa-So@users.noreply.github.com>
2020-10-01 15:48:44 +0000

    deploy: b5fee071cf21f474cde13e6145d03f29288ed582

---
+++ precache-manifest.7cf9c4562fff09aa9c363581ff9f3beb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8d13494bd47a62bd01d755e080f93f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "33534d13ad9a6bb75650",
+    "url": "./static/css/main.702cc51f.chunk.css"
+  },
+  {
+    "revision": "794e800aea99074753e4",
+    "url": "./static/js/2.e46864cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e46864cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33534d13ad9a6bb75650",
+    "url": "./static/js/main.ba338e6a.chunk.js"
+  },
+  {
+    "revision": "3fc6ff4c81cd6a523bee927348ab17ad",
+    "url": "./static/js/main.ba338e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a531b9f0af9beccaf6f2",
+    "url": "./static/js/runtime-main.be3cbd03.js"
+  }
+]);
