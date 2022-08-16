c976089fab7ad9c0ce975e258fdab586d99ea4da
Fahri Sönmez <fahri.soenmez@hbm.com>
2022-08-16 14:02:14 +0200

    fix lint warnings

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import useEvent from '@testing-library/user-event'
-import { InputTags, TagsValue } from '.'
+import { InputTags } from '.'
 
 describe('InputTags', (): void => {
   it('is truthy', (): void => {
